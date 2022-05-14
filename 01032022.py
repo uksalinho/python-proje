c2d04646a2f52dc63e0188f65428df82165d3648
uksalinho <102018998+uksalinho@users.noreply.github.com>
2022-05-14 16:02:00 +0200

    Add files via upload

---
+++ 01032022.py
@@ -0,0 +1,21 @@
+# girdi = input("Birşeyler yazınız : ")
+# girdi2 = 5
+
+# print(girdi)
+# print(girdi2)
+
+###################################################
+
+# print("Ali", "veli", 5, 9, end="\n\n")
+# print("Ali", "veli", 5, 9, sep=", ", end="\n\n")
+# print("Ali" + "veli", 5 + 9, end="\n\n")
+# print("Ali", "veli", "\n")
+
+###################################################
+
+print("Ali", "veli", 5, 9)
+print("Ali" +" "+ "veli", 5 + 9)
+
+# print("Ali" + 5) hatalı kullanım.
+print("Ali" * 5)
+print("Ali" + "Ali" + "Ali" + "Ali" + "Ali")
